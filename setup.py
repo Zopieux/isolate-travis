60a19545c7ab8da4b7eec19e0d2809140fba1def
Alexandre Macabies <web+git@zopieux.com>
2017-08-01 02:19:35 +0200

    lol4

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import subprocess
 print("LOL")
 print(sys.executable)
 
-print(subprocess.check_output('isolate --box-id 10 --init --cg'))
-print(subprocess.check_output('isolate --box-id 10 --cleanup --cg'))
+print(subprocess.check_output('/usr/bin/isolate --box-id 10 --init --cg'))
+print(subprocess.check_output('/usr/bin/isolate --box-id 10 --cleanup --cg'))
 
