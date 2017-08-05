804f8b93b2306dbbd5d6b270ea9270ee85eadc0e
Alexandre Macabies <web+git@zopieux.com>
2017-08-05 18:18:45 +0200

    java lol

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 import sys
 import subprocess
 
-print("LOL")
 print(sys.executable)
 
-print(subprocess.check_output('/usr/bin/isolate --box-id 10 --init --cg'.split()))
+path = subprocess.check_output('/usr/bin/isolate --box-id 10 --init --cg'.split())
+print(path)
+
+with open(path + '/box/Lol.java', 'w') as c:
+    c.write('''
+public class Lol {
+    public static void main(String[] args) {
+        System.out.println("lolz");
+    }
+}''')
+
+print(subprocess.check_output('/usr/bin/isolate --box-id 10 --run --cg -p -d /etc/java-8-openjdk -- /usr/bin/javac Lol.java'.split()))
+print(subprocess.check_output('/usr/bin/isolate --box-id 10 --run --cg -p -d /etc/java-8-openjdk -- /usr/bin/java Lol'.split()))
 print(subprocess.check_output('/usr/bin/isolate --box-id 10 --cleanup --cg'.split()))
 
--- test.sh
+++ test.sh
@@ -7,9 +7,8 @@ if ! [[ $(sudo -E su $USER -c 'groups') == *isolate* ]]; then
    exit 1;
 fi
 
-which python java javac php
+which python
 python --version
 
-sudo -E su $USER -c "set -x; which java javac php"
-sudo -E su $USER -c "set -x; cd $TRAVIS_BUILD_DIR; source $VIRTUAL_ENV/bin/activate; which python; python --version; pwd; ls -al; python setup.py"
+sudo -E su $USER -c "set -x; cd $TRAVIS_BUILD_DIR; source $VIRTUAL_ENV/bin/activate; which python; python setup.py"
 
